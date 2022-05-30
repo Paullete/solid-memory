d6dc6024cee4c99ff2ac938d7445620fd7241edb
paullete <paulettenereah3@gmail.com>
2022-05-30 10:52:59 +0300

    trial

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-88(o)qb%l#4#yz4r0yazx54l$43@5okh04aap)9cgwg@vl@)ap'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['44.200.116.238:3439','127.0.0.1']
+ALLOWED_HOSTS = ['pote-album.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -38,12 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'album.apps.AlbumConfig',
+    'storages',
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
