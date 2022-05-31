64771a481ce1537502ad8dc4d1a4efbedeed60ae
paullete <paulettenereah3@gmail.com>
2022-05-31 18:02:14 +0300

    changes

--- photoshare/photoshare/settings.py
+++ photoshare/photoshare/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'album.apps.AlbumConfig',
-    'storages',
+    
     
 ]
 
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+
     
 ]
 
